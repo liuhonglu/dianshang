436db82fb2780d9d3c40e78163e009c358f07c46
liuhonglu <1197446150@qq.com>
2018-07-11 23:09:38 +0800

    刘鸿禄上传的实训二电商框架

---
+++ dubbo-zookeeper-provider-product/src/main/java/com/jk/service/lsy/impl/adsd.java
@@ -0,0 +1,7 @@
+package com.jk.service.lsy.impl;
+
+/**
+ * Created by Administrator on 2018/7/4 0004.
+ */
+public class adsd {
+}
---
+++ dubbo-zookeeper-provider-product/src/main/java/com/jk/service/lxl/ITreeService.java
@@ -0,0 +1,7 @@
+package com.jk.service.lxl;
+
+/**
+ * Created by Administrator on 2018/7/4 0004.
+ */
+public interface ITreeService {
+}

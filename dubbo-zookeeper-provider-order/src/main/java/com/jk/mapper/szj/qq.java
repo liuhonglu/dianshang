b104fa74fc4e2b56b73df6766acbfb72046d4553
liuhonglu <1197446150@qq.com>
2018-07-11 22:49:04 +0800

    刘鸿禄上传的实训二电商框架

---
+++ dubbo-zookeeper-provider-order/src/main/java/com/jk/mapper/szj/qq.java
@@ -0,0 +1,7 @@
+package com.jk.mapper.szj;
+
+/**
+ * Created by Administrator on 2018/7/5 0005.
+ */
+public class qq {
+}
---
+++ dubbo-zookeeper-provider-order/src/main/java/com/jk/service/hdd/impl/qq.java
@@ -0,0 +1,7 @@
+package com.jk.service.hdd.impl;
+
+/**
+ * Created by Administrator on 2018/7/5 0005.
+ */
+public class qq {
+}
